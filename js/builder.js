5abe599f5602eac545d80d639bcdd22016a5b87b
Patrick Liddy <pjliddy@gmail.com>
2016-08-27 14:49:58 -0400

    auto-generate @variable menus & toggle drop down menu behavior when near bottom of tool panel

--- js/builder.js
+++ js/builder.js
@@ -1,193 +1,13 @@
 // builder.js
 
-var varData = [
-  {
-    name: "brand-primary"
-    , category: "Colors"
-    , subcategory: "Brand Colors"
-    , displayName: "Brand Primary"
-    , value: "#337ab7"
-    , varType: "color"
-    , configType: "function"
-    , displayValue: "darken(#428bca, 6.5%)"
-    , variable: ""
-    , color: "#428bca"
-    , configFunction: "darken"
-    , tint: "6.5"
-    }
-    , {
-    name: "brand-success"
-    , category: "Colors"
-    , subcategory: "Brand Colors"
-    , displayName: "Brand Success"
-    , value: "#5cb85c"
-    , varType: "color"
-    , configType: "colorpicker"
-    , displayValue: "#5cb85c"
-    , variable: ""
-    , color: "#5cb85c"
-    , configFunction: ""
-    , tint: "0"
-    }
-    , {
-    name: "brand-info"
-    , category: "Colors"
-    , subcategory: "Brand Colors"
-    , displayName: "Brand Info"
-    , value: "#5bc0de"
-    , varType: "color"
-    , configType: "colorpicker"
-    , displayValue: "#5bc0de"
-    , variable: ""
-    , color: "#5bc0de"
-    , configFunction: ""
-    , tint: "0"
-    }
-    , {
-    name: "brand-warning"
-    , category: "Colors"
-    , subcategory: "Brand Colors"
-    , displayName: "Brand Warning"
-    , value: "#f0ad4e"
-    , varType: "color"
-    , configType: "colorpicker"
-    , displayValue: "#f0ad4e"
-    , variable: ""
-    , color: "#f0ad4e"
-    , configFunction: ""
-    , tint: "0"
-    }
-    , {
-    name: "brand-danger"
-    , category: "Colors"
-    , subcategory: "Brand Colors"
-    , displayName: "Brand Danger"
-    , value: "#d9534f"
-    , varType: "color"
-    , configType: "colorpicker"
-    , displayValue: "#d9534f"
-    , variable: ""
-    , color: "#d9534f"
-    , configFunction: ""
-    , tint: "0"
-    }
-    , {
-    name: "gray-base"           // variable name for Less
-    , category: "Colors"
-    , subcategory: "Grays"
-    , displayName: "Base Gray"  // name to use in tool panel
-    , value: "#000"             // actual calculated value (use for local CSS)
-    , varType: "color"          // variable type = color
-    , configType: "colorpicker" // config type determines controllor activity based on type
-    , displayValue: "#000000"   // display value field (hex, @variable, or formula)
-    , variable: ""              // @variable if specified
-    , color: "#000000"          // color picked
-    , configFunction: ""        // function chosen
-    , tint: "0"                 // tint value chosen
-    }
-    , {
-    name: "gray-darker"
-    , category: "Colors"
-    , subcategory: "Grays"
-    , displayName: "Darker Gray"
-    , value: "#222"
-    , varType: "color"
-    , configType: "function"
-    , displayValue: "lighten(@gray-base, 13.5%)"
-    , variable: "@gray-base"
-    , color: ""
-    , configFunction: "lighten"
-    , tint: "13.5"
-    }
-  , {
-    name: "gray-dark"
-    , category: "Colors"
-    , subcategory: "Grays"
-    , displayName: "Dark Gray"
-    , value: "#333"
-    , varType: "color"
-    , configType: "function"
-    , displayValue: "lighten(@gray-base, 20%)"
-    , variable: "@gray-base"
-    , color: ""
-    , configFunction: "lighten"
-    , tint: "20"
-    }
-  , {
-    name: "gray"
-    , category: "Colors"
-    , subcategory: "Grays"
-    , displayName: "Gray"
-    , value: "#555"
-    , varType: "color"
-    , configType: "function"
-    , displayValue: "lighten(@gray-base, 33.5%)"
-    , variable: "@gray-base"
-    , color: ""
-    , configFunction: "lighten"
-    , tint: "33.5"
-    }
-  , {
-    name: "gray-light"
-    , category: "Colors"
-    , subcategory: "Grays"
-    , displayName: "Light Gray"
-    , value: "#777"
-    , varType: "color"
-    , configType: "function"
-    , displayValue: "lighten(@gray-base, 46.7%)"
-    , variable: "@gray-base"
-    , color: ""
-    , configFunction: "lighten"
-    , tint: "46.7"
-    }
-  , {
-    name: "gray-lighter"
-    , category: "Colors"
-    , subcategory: "Grays"
-    , displayName: "Lighter Gray"
-    , value: "#eee"
-    , varType: "color"
-    , configType: "function"
-    , displayValue: "lighten(@gray-base, 93.5%)"
-    , variable: "@gray-base"
-    , color: ""
-    , configFunction: "lighten"
-    , tint: "93.5"
-    }
-  ,{
-    name: "body-bg"
-    , category: "Colors"
-    , subcategory: "Scaffolding Colors"
-    , displayName: "Body Background"
-    , value: "#fff"
-    , varType: "color"
-    , configType: "colorpicker"
-    , displayValue: "#ffffff"
-    , variable: ""
-    , color: "#ffffff"
-    , configFunction: ""
-    , tint: "0"
-  }
-  , {
-    name: "text-color"
-    , category: "Colors"
-    , subcategory: "Scaffolding Colors"
-    , displayName: "Text Color"
-    , value: "#333333"
-    , varType: "color"
-    , configType: "colorpicker"
-    , displayValue: "#333333"
-    , variable: ""
-    , color: "#333333"
-    , configFunction: ""
-    , tint: "0"
-    }
-];
+//
+// INIT SCRIPTS
+//
 
-//
-// TEMPLATE SCRIPTS
-//
+// load global variable data
+var varData =  data ;
+
+// LAYOUT TOOL PANEL
 
 var categoryVars = _.filter(varData, function(colorVar){ return colorVar.category == 'Colors'; });
 var categoryTemplate = _.template( $('#category-template').html());
@@ -200,40 +20,8 @@ categoryLayout += categoryTemplate({
 
 $('.tool-panel').append(categoryLayout);
 
-
-function layoutSubcategories ( catVars ){
-  var subcategoryTemplate = _.template( $('#subcategory-template').html());
-  var subcategoryVars = _.toArray(_.groupBy(catVars, function(catVar){ return catVar.subcategory; }));
-  var subcategoryLayout = "";
-
-  _.each( subcategoryVars, function( subcategoryVar, index, subcategoryVars ){
-    var subcategory = subcategoryVar[0].subcategory;
-    subcategoryLayout += subcategoryTemplate({ subcategoryVar: subcategoryVar, subcategory });
-  });
-
-  //alert (JSON.stringify( subcategoryVars ) );
-  return subcategoryLayout;
-}
-
-function layoutVariables( variables ){
-  var varTemplate = _.template( $('#colorvar-template').html());
-  var varLayout = "";
-
-  _.each( variables, function( variable, index, variables ){
-    varLayout += varTemplate({ variable: variable });
-  });
-  return varLayout;
-}
-
-//
-// INITIALIZE APP
-//
-
 $(function () {
-
-  // GET & HANDLE JSON FILE WITH VARS & DATA
-
-  // CREATE UI ELEMENTS
+   // CREATE UI ELEMENTS
 
   // create color pickers
   _(_( varData ).where({ varType: "color" })).each( function( colorVar ){
@@ -249,6 +37,11 @@ $(function () {
   // export link clicked
   $('#link-export').click( generateVariables );
 
+  // set config type menu fly up or down
+  $('.config-type button').on('click', function ( clickEvt ) {
+    setFlyup( clickEvt );
+  });
+
   // color config type menu item selected
   $('.config-type a').click( updateConfigType );
 
@@ -257,8 +50,8 @@ $(function () {
     updateColorPicker( pickerEvt);
   });
 
-  // config variable selected
-  $('.config-variables a').click( updateConfigVariable );
+  // config variable close clicked
+  $('.config-close').click( configClose );
 
   // config slider updated
   $(".config-tint .slider").on("slide", function( slideEvt ) {
@@ -271,18 +64,57 @@ $(function () {
 });
 
 //
+// TEMPLATE SCRIPTS
+//
+
+function layoutSubcategories ( catVars ){
+  var subcategoryTemplate = _.template( $('#subcategory-template').html());
+  var subcategoryVars = _.toArray(_.groupBy(catVars, function(catVar){ return catVar.subcategory; }));
+  var subcategoryLayout = "";
+
+  _.each( subcategoryVars, function( subcategoryVar, index, subcategoryVars ){
+    var subcategory = subcategoryVar[0].subcategory;
+    subcategoryLayout += subcategoryTemplate({ subcategoryVar: subcategoryVar, subcategory });
+  });
+
+  return subcategoryLayout;
+}
+
+function layoutVariables( variables ){
+  var varTemplate = _.template( $('#colorvar-template').html());
+  var varLayout = "";
+
+  _.each( variables, function( variable, index, variables ){
+    varLayout += varTemplate({ variable: variable });
+  });
+  return varLayout;
+}
+
+function layoutVarMenu( varType ){
+  // either bind menu items to variable color values
+  // or generate menu in real time when configType menu chooses "variable"
+  var colorVars = _.filter(varData, function(varObj){ return varObj.category == 'Colors'; });
+  var menuItemTemplate = _.template( $('#varmenu-template').html());
+  var menuLayout = "";
+
+  _.each( colorVars, function( colorVar, index, colorVars ){
+    menuLayout += menuItemTemplate({ colorVar: colorVar });
+  });
+
+  return menuLayout;
+}
+
+//
 // DATA HANDLING FUNCTIONS
 //
 
 function setVar( varID, valueID, value ){
    _(varData).findWhere({ name: varID })[valueID]=value;
-  //myDataObj[valueID]=value;
 }
 
 function getVar(varID, valueID) {
   // finds the varID object with valueID and returns the value property
   return _(_(varData).findWhere({ name: varID })).propertyOf( )( valueID );
-  //return  _.propertyOf( _.findWhere(varData, { name: varID }) )( valueID );
 }
 
 //
@@ -294,7 +126,9 @@ function createColorPicker( varID ) {
   $('#' + varID + ' .color-picker').colorpicker({ color: getVar( varID, 'color'), align: 'left'  });
 }
 
+//
 // COLOR CONFIG EVENT HANDLERS
+//
 
 function updateConfigType(){
   var myVarContainer = $(this).closest('.variable-display');
@@ -311,8 +145,12 @@ function updateConfigType(){
       setVar( myVarID, 'displayValue', getVar(myVarID, 'value'));
       break;
     case 'variable':
+      var menuLayout = layoutVarMenu( 'color' );
+      $(myVarContainer).find('.config-variables ul').append( menuLayout);
       $(myVarContainer).find('.config-variables').show();
       $(myVarContainer).find('.config-tint').hide();
+      // config variable selected
+      $('.config-variables a').click( updateConfigVariable );
       break;
     case 'function':
       $(myVarContainer).find('.config-variables').hide();
@@ -320,8 +158,7 @@ function updateConfigType(){
       setVar( myVarID, 'configType', 'function' );
       setVar( myVarID, 'configFunction', $(this).data('option'));
 
-      updateConfigFunction( $(myVarContainer) ); // should be "update varfunction" function
-
+      updateConfigFunction( $(myVarContainer) );
       break;
   }
   updateDisplayValue( myVarID );
@@ -343,7 +180,6 @@ function updateColorPicker( evt ){
     setVar( myVarID, 'variable', '');
     updateConfigFunction( $(myVarContainer) );
   }
-
   updateDisplayValue( myVarID );
 }
 
@@ -361,7 +197,6 @@ function updateConfigVariable(){
 
   if ( getVar(myVarID, 'configType') == 'function'){
     updateConfigFunction( $(myVarContainer) );
-
   } else {
     // if picker or var, replace all values
     setVar( myVarID, 'configType', 'variable');
@@ -369,7 +204,6 @@ function updateConfigVariable(){
     setVar( myVarID, 'value', colorValue);
     setVar( myVarID, 'displayValue', getVar( myVarID, 'variable'));
   }
-
   updateDisplayValue( myVarID );
 }
 
@@ -420,12 +254,27 @@ function updateColorSwatch ( myVarID ){
   $('#' + myVarID).find('.color-picker').css('background-color', getVar( myVarID, 'value'));
 }
 
+function configClose( ){
+  var myVarContainer = $(this).closest('div').hide();
+}
+
+function setFlyup( clickEvt ){
+  if ( $(window).height() - $( clickEvt.target).offset().top < 200 ){
+    $( clickEvt.target).closest('div').removeClass( "dropdown" );
+    $( clickEvt.target).closest('div').addClass( "dropup" );
+  } else {
+    $( clickEvt.target).closest('div').addClass( "dropdown" );
+    $( clickEvt.target).closest('div').removeClass( "dropup" );
+  }
+}
+
 function updateDisplayValue ( myVarID ){
   $('#' + myVarID).find('.config-display').val( getVar( myVarID, 'displayValue'));
   updateColorSwatch( myVarID );
    $("#layoutframe").contents().find( "." + myVarID ).css( "background-color", getVar( myVarID, 'value') );
-  //showVar( _(varData).where({ name: myVarID }) );
 }
+
+
 
 //
 // LESS/CSS SCRIPTS
